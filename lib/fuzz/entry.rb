d820dbfbebeaca118a7fd2a3ca6e6192a6b1e20b
Harry Schwartz <hello@harryrschwartz.com>
2018-03-02 18:22:21 -0800

    Don't use shortened class names
    This explicitly nests classes inside of modules, rather than using the
    ModuleName::ClassName shortcut.

--- lib/fuzz/entry.rb
+++ lib/fuzz/entry.rb
@@ -1,19 +1,23 @@
-class Fuzz::Entry
-  include Comparable
+module Fuzz
+  class Entry
+    include Comparable
 
-  attr_reader :title, :object, :weight
+    attr_reader :title, :object, :weight
 
-  def initialize(title:, object:, weight:)
-    @title = title
-    @object = object
-    @weight = weight
-  end
+    def initialize(title:, object:, weight:)
+      @title = title
+      @object = object
+      @weight = weight
+    end
 
-  def <=>(other)
-    if weight != other.weight
-      other.weight <=> weight
-    else
-      title <=> other.title
+    def <=>(other)
+      other_weight = other.weight
+
+      if weight != other_weight
+        other_weight <=> weight
+      else
+        title <=> other.title
+      end
     end
   end
 end
--- lib/fuzz/selector.rb
+++ lib/fuzz/selector.rb
@@ -3,48 +3,52 @@ require_relative "entry"
 require_relative "rofi_picker"
 require_relative "null_cache"
 
-class Fuzz::Selector
-  def initialize(items, options = {})
-    @cache = options.fetch(:cache, Fuzz::NullCache.new)
-    @default = options.fetch(:default, nil)
-    @picker = options.fetch(:picker, Fuzz::RofiPicker.new)
-    @entries = items.map { |item| make_entry(item, @cache) }
-  end
-
-  def pick
-    title = picker.pick(titles)
-    chosen_entry = find_entry_by_title(title)
-
-    if chosen_entry.nil?
-      default
-    else
-      cache.increment!(chosen_entry.title)
-      chosen_entry.object
+module Fuzz
+  class Selector
+    def initialize(items, options = {})
+      @cache = options.fetch(:cache, Fuzz::NullCache.new)
+      @default = options.fetch(:default, nil)
+      @picker = options.fetch(:picker, Fuzz::RofiPicker.new)
+      @entries = items.map { |item| make_entry(item, @cache) }
     end
-  end
 
-  private
+    def pick
+      title = picker.pick(titles)
+      chosen_entry = find_entry_by_title(title)
 
-  attr_reader(
-    :cache,
-    :default,
-    :entries,
-    :picker,
-  )
+      if chosen_entry.nil?
+        default
+      else
+        cache.increment(chosen_entry.title)
+        chosen_entry.object
+      end
+    end
 
-  def titles
-    entries.sort.map(&:title)
-  end
+    private
 
-  def find_entry_by_title(title)
-    entries.detect { |entry| entry.title == title }
-  end
-
-  def make_entry(item, cache)
-    Fuzz::Entry.new(
-      title: item.to_s,
-      object: item,
-      weight: cache.weight(item.to_s) || 0,
+    attr_reader(
+      :cache,
+      :default,
+      :entries,
+      :picker,
     )
+
+    def titles
+      entries.sort.map(&:title)
+    end
+
+    def find_entry_by_title(title)
+      entries.detect { |entry| entry.title == title }
+    end
+
+    def make_entry(item, cache)
+      title = item.to_s
+
+      Fuzz::Entry.new(
+        title: title,
+        object: item,
+        weight: cache.weight(title) || 0,
+      )
+    end
   end
 end
